23467c3fe4be3fba80baeb2fc20f2d9b40832ada
SSa1nt <39455909+SSa1nt@users.noreply.github.com>
2021-04-30 10:05:23 +0100

    Added doll script / SONIC UPDATE

---
+++ DoorsOfLife/Assets/Scripts/Utilities/Puzzle/DollPuzzle/DollPuzzleMaster.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DollPuzzleMaster : MonoBehaviour
+{
+    [SerializeField]
+    private PuzzlePiece[] myPieces;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
