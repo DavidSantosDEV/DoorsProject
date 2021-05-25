6728b3eea6ca1b1cb0f2011e96a3b78edbcddcf4
SSa1nt <39455909+SSa1nt@users.noreply.github.com>
2021-05-25 22:28:43 +0100

    NPC first steps

---
+++ DoorsOfLife/Assets/Scripts/Utilities/NPC/DollNPC.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+[System.Serializable]
+public class StringListProgression //I considered a struct but then I couldnt do the initialization 
+{
+    public bool showImage=true;
+    public bool showName=true;
+    public string[] dialogue;
+}
+
+public class DollNPC : IInteractibleBase
+{
+    private AudioSource myAudioSource = null;
+
+    private TextMeshProUGUI textDisplay = null;
+    private TextMeshProUGUI textNameDisplay = null;
+
+    [SerializeField]
+    [Range(0, 0.2f)]
+    private float textTypeSpeed = 0.2f;
+
+    [Header("Dialogue Settings")]
+    [SerializeField]
+    private bool showImage = true;
+    [SerializeField]
+    private bool showName = true;
+    [SerializeField]
+    private string displaySpeakerName = "?";
+    [SerializeField]
+    private Sprite displayAvatar;
+    //[SerializeField]
+    //private string[] sentences;
+    [SerializeField]
+    DialogAndEvent[] SentencesAndEvents;
+
+
+
+    [Header("NPC Settings")]
+    [SerializeField][ShowOnly]
+    private int progressionLevel=0;
+    [SerializeField]
+    private string nameSet = "Doll";
+
+    [SerializeField]
+    private StringListProgression[] levelAndStrings;
+
+    public int ProgressionNPC
+    {
+        get => progressionLevel;
+        set
+        {
+            if (value >= 0 && !(levelAndStrings.Length<value))
+            {
+                progressionLevel = value;
+            }
+            else
+            {
+                progressionLevel = 0;
+            }
+        }
+    }
+
+    private void Awake()
+    {
+        isInteractible = true; //Needs to start interactible;
+        myAudioSource = GetComponent<AudioSource>();
+    }
+
+    public override void OnInteract()
+    {
+        /*switch (progressionLevel)
+        {
+            case 0:
+
+                break;
+        }*/
+        base.OnInteract();
+    }
+
+
+}
